c161832206dbd0566dcf5d33493103f77f6330ed
Tony O' Hare <tony@hobocore.info>
2019-02-14 20:08:53 +0000

    200 & 404 tests

--- tests/Unit/RedirectTest.php
+++ tests/Unit/RedirectTest.php
@@ -3,14 +3,47 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Redirect;
+use Jchook\AssertThrows\AssertThrows;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-/**
- * 
- */
 class RedirectTests extends TestCase
 {
+    use RefreshDatabase;
+    use AssertThrows;
+
+    const GOOGLE = "https://google.com";
+    const YAHOO = "https://yahoo.com";
+    const ALTA_VISTA = "http://altavista.com";
+
+    protected $redirect;
+
+    protected function setUp() {
+        parent::setUp();
+
+       $this -> createANewRedirect();
+    }
+
+    private function createANewRedirect() {
+        $startData = [
+            "old" => self::YAHOO, 
+            "new" => self::GOOGLE,
+            "code" => 302
+        ];
+        $this -> redirect = new Redirect($startData);
+        $this -> redirect -> save();
+    }
     
-    function testBasic() {
-        $this -> assertEquals(1,1);
+    function test_Throws404_WhenOldDoesntExist() {
+        $this->assertThrows(ModelNotFoundException::class, function() {
+            Redirect::for(self::ALTA_VISTA);
+        });
+    }
+
+    function test_ReturnsNew_WhenOldExists() {
+        $new = Redirect::for(self::YAHOO);
+
+        $this -> assertEquals(self::GOOGLE, $new);
     }
 }
